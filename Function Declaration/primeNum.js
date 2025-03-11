819787418de89f9eff131a1a3c252ddeb4cc336f
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-11 10:27:36 +0530

    Add files via upload

--- Function Declaration/primeNum.js
+++ Function Declaration/primeNum.js
@@ -2,23 +2,24 @@
 // Check Prime Number:
 // Create a function declaration that checks if a given number is a prime number.
 
-function prime(n){
+
+function isPrime(n){
    if(n<2){
-      console.log("n is not a prime number")
+      console.log("n is a composite number")
       return;
    }
-   for(let i = 2; i<=Math.sqrt(n);i++){
-    if(n%i==0){
-        console.log("n is not a prime number")
-        return ;
-    }
 
+   for(let i = 0; i<=n/2; i++){
+      if(n%i==0){
+         console.log(n+" is not a prime number");
+         return;
+      }
+      else{
+         console.log("n is a prime number")
+      }
    }
-  console.log(n +  "is prime number ")
-
 }
-
-prime(67)
-
+ isPrime(12)
 
 
+//  console.log(Math.sqrt(19))
