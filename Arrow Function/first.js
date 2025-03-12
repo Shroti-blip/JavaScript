5d5546507b95ef456c8d42d35be8fa55ab6f7f82
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-13 00:05:56 +0530

    Add files via upload

---
+++ Arrow Function/first.js
@@ -0,0 +1,14 @@
+// Square of Numbers:
+// Write an arrow function that takes an array of numbers and returns a new array with each number squared.
+
+// Check Even or Odd:
+// Create an arrow function that takes a number as input and returns "Even" or "Odd".
+
+// Filter Strings by Length:
+// Write an arrow function that filters an array of strings, returning only the strings with a length greater than 3.
+
+// Sum of Array Elements:
+// Create an arrow function that calculates the sum of all elements in an array.
+
+// Reverse a String:
+// Write an arrow function that takes a string and returns it reversed.
