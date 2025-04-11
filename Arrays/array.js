1ccac9d3ecfb6fdf6be4fca9a9984e7c743571dc
Muskan Shroti <shrotimuskan@gmail.com>
2025-04-12 00:47:54 +0530

    Add files via upload

---
+++ Arrays/array.js
@@ -0,0 +1,18 @@
+arr = new Array("a1","a2","a3");
+console.log(arr)
+ let arr2 = [1,2,3,4,5]
+ console.log(arr2)
+ console.log(arr2[0])
+ console.log(arr2[1])
+ console.log(arr2[2])
+
+ console.log("-------------")
+ console.log(arr2.push(22))
+ console.log(arr2)
+ console.log(arr2.pop())//last element take out
+ console.log(arr2)
+ console.log("--------------")
+ arr3 = new Array(5)
+ console.log(arr3.length)
+
+ 
